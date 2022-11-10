f10e7d7d439c756bfd261fc051f98c16da8b6fe3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-10 17:16:16 -0300

    chore: init develompent

---
+++ README.md
@@ -0,0 +1,6 @@
+# Formatch
+
+Color Tokens:
+
+- **main.500**: #000C7C _blue_
+- **secondary.500**: #FA5D14 _orange_
---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { Center } from 'native-base'
+
+import Logotipo from '../assets/logotipo.svg'
+
+export function Home() {
+  return (
+    <Center flex={1}>
+      <Logotipo width={230.65} height={40} />
+    </Center>
+  )
+}
