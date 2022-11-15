a7f2d3efa37d56872b98ef2e941690ea29d5e35d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-15 10:41:57 -0300

    feat(ui/ux): improve bottom tab navigation
    * Remove default header
    * Add active/inactive icon colors
    * Change tab bar style
    * Add icons for each screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useTheme } from 'native-base'
+import { Article, MagnifyingGlass, GearSix } from 'phosphor-react-native'
+
 import { Feed } from '../screens/Feed'
 import { ListProfessionals } from '../screens/ListProfessionals'
 import { Options } from '../screens/Options'
@@ -6,11 +9,51 @@ import { Options } from '../screens/Options'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes() {
+  const { colors, sizes } = useTheme()
+
   return (
-    <Navigator>
-      <Screen name='feed' component={Feed} />
-      <Screen name='list-professionals' component={ListProfessionals} />
-      <Screen name='options' component={Options} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.primary[500],
+        tabBarInactiveTintColor: colors.complement[500],
+        tabBarStyle: {
+          position: 'absolute',
+          height: sizes[22],
+          borderTopWidth: 0,
+          elevation: 100,
+          backgroundColor: colors.red[300],
+        },
+      }}
+    >
+      <Screen
+        name='feed'
+        component={Feed}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Article color={color} size={35} weight='duotone' />
+          ),
+        }}
+      />
+      <Screen
+        name='list-professionals'
+        component={ListProfessionals}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MagnifyingGlass color={color} size={35} weight='duotone' />
+          ),
+        }}
+      />
+      <Screen
+        name='options'
+        component={Options}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <GearSix color={color} size={35} weight='duotone' />
+          ),
+        }}
+      />
     </Navigator>
   )
 }
