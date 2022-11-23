3e83edf99d40528abd7c8e4c4519a554ac1024ca
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-23 14:30:33 -0300

    feat(ui): import fonts on App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
 import { Routes } from './src/routes'
+import { Loading } from './src/components/Loading'
+
 import { THEME } from './src/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
@@ -10,7 +23,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      <Routes />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
