e732bb1e829dafec16e35d46712f4caeae011dee
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-25 15:57:48 -0300

    feat: create ArticleList component

---
+++ src/components/ArticleList.tsx
@@ -0,0 +1,33 @@
+import { useTheme, VStack } from 'native-base'
+
+import articles from '../assets/articles.json'
+import { ArticleCard } from './ArticleCard'
+
+export function ArticleList() {
+  const { sizes } = useTheme()
+
+  return (
+    <VStack pb={sizes[22] * 2} px='5'>
+      {articles.map((article) => {
+        if (article.adMeta) {
+          return (
+            <ArticleCard
+              key={article.slug}
+              title={article.title}
+              imageBannerUrl={article.bannerUrl}
+              adMeta={article.adMeta}
+            />
+          )
+        } else {
+          return (
+            <ArticleCard
+              key={article.slug}
+              title={article.title}
+              imageBannerUrl={article.bannerUrl}
+            />
+          )
+        }
+      })}
+    </VStack>
+  )
+}
