2d0733ffadab7ecf24647718cf4d040a2ceb8a7d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-11-25 15:57:53 -0300

    feat: add articles on Feed screen

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,40 +1,16 @@
-import { Box, ScrollView, Text, useTheme, VStack } from 'native-base'
+import { ScrollView, VStack } from 'native-base'
 import { Header } from '../components/Header'
 import { Title } from '../components/Title'
 
-const TmpCardComponent = () => (
-  <Box
-    borderWidth={4}
-    borderColor='secondary.500'
-    backgroundColor='secondary.200'
-    px='10'
-    py='7'
-    mx='5'
-    borderRadius='3xl'
-    mb='5'
-  >
-    <Text fontFamily='bold' fontSize='5xl'>
-      Feed
-    </Text>
-  </Box>
-)
+import { ArticleList } from '../components/ArticleList'
 
 export function Feed() {
-  const { sizes } = useTheme()
-
   return (
     <VStack flex={1} backgroundColor='background.500'>
       <ScrollView>
         <Header />
         <Title text='Novidades e dicas' />
-        <VStack pb={sizes[22] * 2}>
-          <TmpCardComponent />
-          <TmpCardComponent />
-          <TmpCardComponent />
-          <TmpCardComponent />
-          <TmpCardComponent />
-          <TmpCardComponent />
-        </VStack>
+        <ArticleList />
       </ScrollView>
     </VStack>
   )
