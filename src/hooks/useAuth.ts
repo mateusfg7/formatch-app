ce9c3bb6e8b9b53f3133886fa6851b7b69236f7e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-11 17:14:26 -0300

    feat(auth): create `useAuth` hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,4 @@
+import { useContext } from 'react'
+import { AuthContext, AuthContextDataProps } from '../contexts/AuthContext'
+
+export const useAuth = (): AuthContextDataProps => useContext(AuthContext)
