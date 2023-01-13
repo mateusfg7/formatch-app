a9d615ab2b5554aac88accf2776f383a29820734
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-13 12:51:25 -0300

    feat: small update on Options screen
    * Convert js values to screen
    * Use `w=full` for HStacks

--- src/screens/Options.tsx
+++ src/screens/Options.tsx
@@ -22,25 +22,25 @@ export function Options() {
           borderRadius='full'
           mb='6'
         />
-        <HStack alignItems='center' mb='3'>
+        <HStack alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>nome: </Text>
           <Text fontSize='xl'>{user.name}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3'>
+        <HStack alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>email: </Text>
           <Text fontSize='xl'>{user.email}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3'>
+        <HStack alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>premium: </Text>
-          <Text fontSize='xl'>{user.subscribe}</Text>
+          <Text fontSize='xl'>{String(user.subscribe)}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3'>
+        <HStack alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>environment: </Text>
-          <Text fontSize='xl'>{Constants.appOwnership}</Text>
+          <Text fontSize='xl'>{String(Constants.appOwnership)}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3'>
+        <HStack alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>system version: </Text>
-          <Text fontSize='xl'>{Constants.systemVersion}</Text>
+          <Text fontSize='xl'>{String(Constants.systemVersion)}</Text>
         </HStack>
       </VStack>
     </VStack>
