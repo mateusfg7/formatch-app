e552b64c86bdf54348307d6486b015112813c139
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-12 22:30:45 -0300

    feat: add EXPO_CLIENT_ID and ANDROID_CLIENT_ID variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const EXPO_CLIENT_ID = process.env.EXPO_CLIENT_ID
+const ANDROID_CLIENT_ID = process.env.ANDROID_CLIENT_ID
+
 const API_URL = process.env.API_URL
 
-export { GOOGLE_CLIENT_ID, API_URL }
+export { EXPO_CLIENT_ID, ANDROID_CLIENT_ID, API_URL }
