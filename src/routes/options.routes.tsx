275e1869cc9d975aec2caefa19cf433b250af841
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-13 20:58:37 -0300

    feat(routes): add Profile screen on Options routes

--- src/routes/options.routes.tsx
+++ src/routes/options.routes.tsx
@@ -1,9 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import { Options } from '../screens/Options'
+import { Profile } from '../screens/Options/Profile'
 
 export type OptionsNavigatorParamList = {
   'options.home': {}
+  'options.profile': {}
 }
 
 const { Navigator, Screen } =
@@ -13,6 +15,7 @@ export function OptionsRoute() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name='options.home' component={Options} />
+      <Screen name='options.profile' component={Profile} />
     </Navigator>
   )
 }
