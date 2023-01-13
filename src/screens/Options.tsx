ba9a588e642db8a8213a5573841215ca53011314
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-13 15:54:50 -0300

    feat: small update on Options screen
    * Add jsCompiler info

--- src/screens/Options.tsx
+++ src/screens/Options.tsx
@@ -22,25 +22,31 @@ export function Options() {
           borderRadius='full'
           mb='6'
         />
-        <HStack alignItems='center' mb='3' w='full'>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>nome: </Text>
           <Text fontSize='xl'>{user.name}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3' w='full'>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>email: </Text>
           <Text fontSize='xl'>{user.email}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3' w='full'>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>premium: </Text>
           <Text fontSize='xl'>{String(user.subscribe)}</Text>
         </HStack>
-        <HStack alignItems='center' mb='3' w='full'>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>environment: </Text>
-          <Text fontSize='xl'>{String(Constants.appOwnership)}</Text>
+          <Text fontSize='xl'>
+            {String(Constants.appOwnership) ?? 'standalone'}
+          </Text>
         </HStack>
-        <HStack alignItems='center' mb='3' w='full'>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
           <Text fontSize='lg'>system version: </Text>
           <Text fontSize='xl'>{String(Constants.systemVersion)}</Text>
+        </HStack>
+        <HStack justifyContent='flex-start' alignItems='center' mb='3' w='full'>
+          <Text fontSize='lg'>js engine: </Text>
+          <Text fontSize='xl'>{String(Constants.expoConfig.jsEngine)}</Text>
         </HStack>
       </VStack>
     </VStack>
