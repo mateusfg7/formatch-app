137309259e4d8668f6aa49e195e1f7a573065d87
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-15 18:35:57 -0300

    feat(auth): check user on storage before show sign in screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,33 @@ import { useAuth } from '../hooks/useAuth'
 
 import { SignIn } from '../screens/SignIn'
 import { AppRoutes } from './app.routes'
+import { useEffect, useState } from 'react'
+import { Loading } from '../components/Loading'
 
 export function Routes() {
-  const { user } = useAuth()
+  const [checkUser, setCheckUser] = useState(false)
+
+  const { user, checkUserOnStorage } = useAuth()
+
+  useEffect(() => {
+    async function init() {
+      setCheckUser(true)
+      await checkUserOnStorage()
+      setCheckUser(false)
+    }
+
+    init()
+  }, [])
 
   return (
     <Box flex='1'>
-      <NavigationContainer>
-        {user.name ? <AppRoutes /> : <SignIn />}
-      </NavigationContainer>
+      {checkUser ? (
+        <Loading />
+      ) : (
+        <NavigationContainer>
+          {user.name ? <AppRoutes /> : <SignIn />}
+        </NavigationContainer>
+      )}
     </Box>
   )
 }
