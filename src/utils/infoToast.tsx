33dc70b2976c17c4112bd9cd87e28d4b165a485a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-15 18:31:21 -0300

    feat(utils): create infoToast

---
+++ src/utils/infoToast.tsx
@@ -0,0 +1,23 @@
+import { HStack, Text, Toast } from 'native-base'
+import { Info, XCircle } from 'phosphor-react-native'
+
+export function infoToast(message: string) {
+  Toast.show({
+    render: () => (
+      <HStack
+        borderWidth={1}
+        borderColor='blue.900'
+        bg='blue.100'
+        borderRadius='2xl'
+        p='5'
+        alignItems='center'
+        justifyContent='space-between'
+      >
+        <Info />
+        <Text fontSize='md' ml='3'>
+          {message}
+        </Text>
+      </HStack>
+    ),
+  })
+}
