cc0584717aaf169463587b4644c721338852055b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-23 16:00:21 -0300

    feat(ui): update `Form/Submit` button
    Add loading styles

--- src/components/Form/Submit.tsx
+++ src/components/Form/Submit.tsx
@@ -1,4 +1,4 @@
-import { HStack } from 'native-base'
+import { HStack, Spinner } from 'native-base'
 import {
   Button as NativeBaseButton,
   IButtonProps,
@@ -8,7 +8,11 @@ import {
 } from 'native-base'
 import { IdentificationBadge } from 'phosphor-react-native'
 
-export function Submit({ disabled = false, ...rest }: IButtonProps) {
+export function Submit({
+  disabled = false,
+  isLoading = false,
+  ...rest
+}: IButtonProps) {
   const { colors, fontSizes } = useTheme()
 
   return (
@@ -21,17 +25,25 @@ export function Submit({ disabled = false, ...rest }: IButtonProps) {
       borderColor={disabled ? 'complement.400' : 'primary.500'}
       borderRadius='3xl'
       _pressed={{
-        bg: disabled ? 'rgba(84, 84, 87, 0.07)' : 'primary.700',
+        bg: disabled
+          ? 'rgba(84, 84, 87, 0.07)'
+          : isLoading
+          ? 'primary.800'
+          : 'primary.700',
       }}
       {...rest}
     >
       <HStack alignItems='center'>
         <Box mr='3'>
-          <IdentificationBadge
-            size={fontSizes['4xl']}
-            color={disabled ? colors.complement[400] : colors.primary[500]}
-            weight='duotone'
-          />
+          {isLoading ? (
+            <Spinner size={fontSizes['4xl']} color={colors.primary[500]} />
+          ) : (
+            <IdentificationBadge
+              size={fontSizes['4xl']}
+              color={disabled ? colors.complement[400] : colors.primary[500]}
+              weight='duotone'
+            />
+          )}
         </Box>
         <Text
           color={disabled ? 'complement.400' : 'primary.500'}
