875c1848d5657e99335b31008e9cfe4b3b1d0525
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-02 11:37:47 -0300

    feat: use professional context on `Register` screen

--- src/screens/Options/Register/index.tsx
+++ src/screens/Options/Register/index.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react'
 import { RegisterForm } from './RegisterForm'
+import { UserRegisteredAsProfesisonal } from './UserRegisteredAsProfessional'
+import { Loading } from '../../../components/Loading'
+import { useProfessional } from '../../../hooks/useProfessional'
+import { Box } from 'native-base'
+import { Header } from '../../../components/Header'
 
 export function Register() {
-  return <RegisterForm />
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { professionalData, getUserAsProfessional } = useProfessional()
+
+  useEffect(() => {
+    ;(async () => {
+      if (!professionalData.code) {
+        setIsLoading(true)
+        await getUserAsProfessional()
+        setIsLoading(false)
+      } else {
+        setIsLoading(false)
+      }
+    })()
+  }, [])
+
+  return (
+    <Box flex='1' bg='background.500'>
+      {isLoading ? (
+        <>
+          <Header showBackButton />
+          <Loading />
+        </>
+      ) : professionalData.code ? (
+        <UserRegisteredAsProfesisonal />
+      ) : (
+        <RegisterForm />
+      )}
+    </Box>
+  )
 }
