c6cedb8711076020ba2712c9777aa992de5a51cd
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-08 16:16:57 -0300

    feat: connect Article list with API

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,35 +1,82 @@
-import { useTheme, VStack } from 'native-base'
+import { useEffect, useState } from 'react'
+import { Box, Center, Spinner, Text, useTheme, VStack } from 'native-base'
 
-import articles from '../assets/articles.json'
 import { ArticleCard } from './ArticleCard'
 
+import FileSearchingRafiki from '../assets/file-searching-rafiki.svg'
+import { api } from '../services/api'
+import { AxiosError } from 'axios'
+import { feedbackToast } from '../utils/feedbackToast'
+import { AxiosRequestErrorInfo } from './AxiosRequestErrorInfo'
+
+interface ArticleData {
+  title: string
+  slug: string
+  banner_url: string
+  content: string
+  createdAt: string
+  AdMeta?: {
+    name: string
+    logo_url: string
+    website_url: string
+  }
+}
+
 export function ArticleList() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<AxiosError | undefined>(undefined)
+  const [articles, setArticles] = useState<ArticleData[] | undefined>()
+
   const { sizes } = useTheme()
+
+  async function fetchArticles() {
+    await api
+      .get<ArticleData[]>('/article/list')
+      .then((response) => setArticles(response.data))
+      .catch((error: AxiosError) => {
+        console.log(JSON.stringify(error))
+        setError(error)
+        feedbackToast(
+          'ERROR',
+          'Ocorreu um erro durante o carregamento de artigos'
+        )
+      })
+      .finally(() => setIsLoading(false))
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
 
   return (
     <VStack pb={sizes[22] * 2} px='5'>
-      {articles.map((article) => {
-        if (article.adMeta) {
-          return (
-            <ArticleCard
-              key={article.slug}
-              title={article.title}
-              slug={article.slug}
-              imageBannerUrl={article.bannerUrl}
-              adMeta={article.adMeta}
-            />
-          )
-        } else {
-          return (
-            <ArticleCard
-              key={article.slug}
-              slug={article.slug}
-              title={article.title}
-              imageBannerUrl={article.bannerUrl}
-            />
-          )
-        }
-      })}
+      {isLoading && (
+        <Center flex='1' py='20'>
+          <Spinner size='lg' color='complement.500' />
+        </Center>
+      )}
+      {!error &&
+        !isLoading &&
+        articles &&
+        (articles.length > 0 ? (
+          articles.map((article) => (
+            <ArticleCard key={article.slug} article={article} />
+          ))
+        ) : (
+          <Center py='20'>
+            <Box opacity='0.7' mb='3'>
+              <FileSearchingRafiki width='230' height='177.56' />
+            </Box>
+            <Text fontSize='lg' color='complement.300' bold>
+              Sem artigos disponíveis
+            </Text>
+          </Center>
+        ))}
+
+      {!isLoading && error && <AxiosRequestErrorInfo error={error} />}
+      {!error && !isLoading && !articles && (
+        <Text>Erro ao carregar artigos.</Text>
+      )}
     </VStack>
   )
 }
