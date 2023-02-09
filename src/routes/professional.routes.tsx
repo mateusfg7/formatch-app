6efa1a4de8ad75da01ac9596e32a9b38262cba89
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-09 14:52:10 -0300

    feat(routes): add ProfessionalDetails on routes

--- src/routes/professional.routes.tsx
+++ src/routes/professional.routes.tsx
@@ -4,11 +4,13 @@ import { SelectService } from '../screens/SearchProfessional/SelectService'
 import { SelectLocation } from '../screens/SearchProfessional/SelectLocation'
 import { ProfessionalList } from '../screens/SearchProfessional/ProfessionalList'
 import { ProfessionalSearchContextProvider } from '../contexts/ProfessionalSearchContext'
+import { ProfessionalDetails } from '../screens/SearchProfessional/ProfessionalDetails'
 
 export type ProfessionalNavigatorParamList = {
   'professional.location': {}
-  'professional.service': { uf: string; city: string }
-  'professional.list': { uf: string; city: string; service: string }
+  'professional.service': {}
+  'professional.list': {}
+  'professional.details': { code: string }
 }
 
 const { Navigator, Screen } =
@@ -21,6 +23,7 @@ export function ProfessionalRoute() {
         <Screen name='professional.location' component={SelectLocation} />
         <Screen name='professional.service' component={SelectService} />
         <Screen name='professional.list' component={ProfessionalList} />
+        <Screen name='professional.details' component={ProfessionalDetails} />
       </Navigator>
     </ProfessionalSearchContextProvider>
   )
