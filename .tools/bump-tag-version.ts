079cc5c274fcaf9bfdc1ec30dd88abe0965ce0fa
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-03 15:21:08 -0300

    chore(tools): update bump-tag-version tool
    Export GITHUB_OUTPUT variable with the version state

--- .tools/bump-tag-version.ts
+++ .tools/bump-tag-version.ts
@@ -1,17 +1,17 @@
 // @deno-types="npm:@types/semver"
-import semver from "npm:semver";
-import { simpleGit, SimpleGit } from 'npm:simple-git';
-import packageJson from '../package.json' assert {type: 'json' }
+import semver from 'npm:semver'
+import { simpleGit, SimpleGit } from 'npm:simple-git'
+import packageJson from '../package.json' assert { type: 'json' }
 
 function log(text: string) {
   console.log(`LOG | ${text}`)
 }
 
-const simplegit: SimpleGit = simpleGit(Deno.cwd());
+const simplegit: SimpleGit = simpleGit(Deno.cwd())
 
-const tags = await simplegit.tags();
-const lastTag = tags.latest;
-const packageVersion = packageJson.version;
+const tags = await simplegit.tags()
+const lastTag = tags.latest
+const packageVersion = packageJson.version
 
 if (lastTag) {
   if (semver.gt(packageVersion, lastTag)) {
@@ -20,8 +20,15 @@ if (lastTag) {
 
     log('Pushing tags')
     await simplegit.pushTags()
+
+    Deno.run({
+      cmd: ['echo', 'isNew=true', '>>', '$GITHUB_OUTPUT'],
+    })
   } else {
     log('Last tag and package version are synchronized')
+    Deno.run({
+      cmd: ['echo', 'isNew=false', '>>', '$GITHUB_OUTPUT'],
+    })
   }
 } else {
   log('There is no latest tag')
