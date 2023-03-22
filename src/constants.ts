e6185718b008fe8dfaaf8165d86a8d416bad47e4
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-22 12:38:48 -0300

    refactor(clean): remove unused `EXPO_CLIENT_ID` on constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-const EXPO_CLIENT_ID = process.env.EXPO_CLIENT_ID
 const ANDROID_CLIENT_ID =
   process.env.BUILD_ENV === 'production'
     ? process.env.ANDROID_CLIENT_ID_PROD
@@ -6,4 +5,4 @@ const ANDROID_CLIENT_ID =
 
 const API_URL = process.env.API_URL
 
-export { EXPO_CLIENT_ID, ANDROID_CLIENT_ID, API_URL }
+export { ANDROID_CLIENT_ID, API_URL }
