2fd51a79db98137b711e9caf2d5c419cb0becb65
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-22 12:37:40 -0300

    feat(env): use google client id based on build env

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 const EXPO_CLIENT_ID = process.env.EXPO_CLIENT_ID
-const ANDROID_CLIENT_ID = process.env.ANDROID_CLIENT_ID
+const ANDROID_CLIENT_ID =
+  process.env.BUILD_ENV === 'production'
+    ? process.env.ANDROID_CLIENT_ID_PROD
+    : process.env.ANDROID_CLIENT_ID_DEV
 
 const API_URL = process.env.API_URL
 
