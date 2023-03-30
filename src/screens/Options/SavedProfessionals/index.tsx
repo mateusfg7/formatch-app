e2d880aa3332df84a0caa9e75f831461413a7e7c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-30 10:48:23 -0300

    refactor: use ProfessionalCard component on saved professional list

--- src/screens/Options/SavedProfessionals/index.tsx
+++ src/screens/Options/SavedProfessionals/index.tsx
@@ -1,21 +1,5 @@
 import { useEffect, useState } from 'react'
-import {
-  VStack,
-  ScrollView,
-  HStack,
-  Text,
-  Box,
-  Center,
-  Pressable,
-  Image,
-  Spinner,
-} from 'native-base'
-import {
-  Envelope,
-  InstagramLogo,
-  Phone,
-  WhatsappLogo,
-} from 'phosphor-react-native'
+import { VStack, ScrollView, Text, Center, Spinner } from 'native-base'
 import { AxiosError } from 'axios'
 
 import { api } from '../../../services/api'
@@ -28,21 +12,7 @@ import { Title } from '../../../components/Title'
 import { AxiosRequestErrorInfo } from '../../../components/AxiosRequestErrorInfo'
 
 import HouseSearch from '../../../assets/house-search.svg'
-
-interface ProfessionalData {
-  code: string
-  profile_picture_url: string
-  name: string
-  email: string | null
-  phone: string | null
-  state_uf: string
-  city: string
-  biography: string
-  whatsapp: string | null
-  instagram: string | null
-  createdAt: string
-  updatedAt: string
-}
+import { ProfessionalCard } from '../../../components/ProfessionalCard'
 
 export function SavedProfessionals() {
   const [isLoading, setIsLoading] = useState(true)
@@ -51,7 +21,7 @@ export function SavedProfessionals() {
     ProfessionalData[] | undefined
   >(undefined)
 
-  const { navigate, addListener, removeListener } = optionsNavigation()
+  const { navigate, addListener } = optionsNavigation()
 
   async function fetchProfessionalList() {
     await api
@@ -106,62 +76,16 @@ export function SavedProfessionals() {
           <ScrollView>
             <VStack pb='40'>
               {professionalList.map((professional) => (
-                <Pressable
-                  key={professional.name}
+                <ProfessionalCard
+                  key={professional.code}
+                  professional={professional}
                   onPress={() =>
                     navigate('options.saved.details', {
                       code: professional.code,
                     })
                   }
-                >
-                  <HStack
-                    bg='white'
-                    alignItems='center'
-                    shadow='2'
-                    rounded='3xl'
-                    my='2 '
-                    mx='5'
-                    overflow='hidden'
-                  >
-                    <Center size='110'>
-                      <Image
-                        source={{ uri: professional.profile_picture_url }}
-                        alt='Picture'
-                        size='110'
-                      />
-                    </Center>
-                    <VStack
-                      flex='1'
-                      alignItems='flex-start'
-                      justifyContent='space-around'
-                      p='2'
-                      space='4'
-                    >
-                      <Box w='full'>
-                        <Text
-                          fontSize='lg'
-                          color='complement.500'
-                          fontFamily='bold'
-                          isTruncated
-                        >
-                          {professional.name}
-                        </Text>
-                      </Box>
-                      <HStack alignItems='center' space='4'>
-                        <HStack alignItems='center' space='2'>
-                          {professional.whatsapp && (
-                            <WhatsappLogo weight='light' />
-                          )}
-                          {professional.instagram && (
-                            <InstagramLogo weight='light' />
-                          )}
-                          {professional.phone && <Phone weight='light' />}
-                          {professional.email && <Envelope weight='light' />}
-                        </HStack>
-                      </HStack>
-                    </VStack>
-                  </HStack>
-                </Pressable>
+                  hideRating
+                />
               ))}
             </VStack>
           </ScrollView>
