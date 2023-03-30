ae9bb40f152156ecfee0abe106e8b33678b37c8b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-30 10:56:29 -0300

    feat(ui/ux): add loading skeleton to professional picture on professional details screen

--- src/components/ProfessionalInfo/ProfessionalImage.tsx
+++ src/components/ProfessionalInfo/ProfessionalImage.tsx
@@ -1,5 +1,20 @@
+import { useState } from 'react'
+import { Box } from 'native-base'
 import { Image, IImageProps } from 'native-base'
+import { LoadingSkeleton } from '../LoadingSkeleton'
 
 export const ProfessionalImage = ({ ...props }: IImageProps) => {
-  return <Image w='full' h='64' borderRadius='3xl' {...props} />
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+
+  return (
+    <Box w='full' h='64' borderRadius='3xl' overflow='hidden'>
+      <LoadingSkeleton isContentVisible={isImageLoaded}>
+        <Image
+          size='full'
+          onLoadEnd={() => setIsImageLoaded(true)}
+          {...props}
+        />
+      </LoadingSkeleton>
+    </Box>
+  )
 }
